7f1a4dee5a5dae69c488028c0be6c64ec7ad0fe5
Nejikugi <63852665+scrwnl@users.noreply.github.com>
2024-06-02 23:27:23 +0900

    wip

---
+++ src/main/java/io/github/scrwnl/ameliefun/AmelieFunInterpreter.java
@@ -0,0 +1,41 @@
+package io.github.scrwnl.ameliefun;
+
+import org.kohsuke.args4j.*;
+/**
+ * Hello world!
+ *
+ */
+public class AmelieFunInterpreter
+{
+    @Option(name = "--version", aliases = "-v", usage = "Show version")
+    private boolean versionFlag;
+
+    @Option(name = "--help", aliases = "-h", usage = "Show help")
+    private boolean helpFlag;
+
+    //@Option(name = "--debug", usage = "Debug mode")
+    //private boolean isDebugMode;
+
+    @Argument(index = 0, metaVar = "program file")
+    private String fileName;
+
+    public static void main( String[] args )
+    {
+        AmelieFunInterpreter commandLine = new AmelieFunInterpreter();
+        CmdLineParser parser = new CmdLineParser(commandLine);
+        try {
+            parser.parseArgument(args);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            System.exit(1);
+        }
+
+        if ( commandLine.versionFlag ) {
+            System.out.println( "AmelieFun Interpreter version 1.0.0" );
+            System.exit(0);
+        } else if ( commandLine.helpFlag ) {
+            System.out.println( "Show Usage" );
+            System.exit(0);
+        }
+    }
+}
